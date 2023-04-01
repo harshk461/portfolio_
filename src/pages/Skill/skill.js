72a58ffb20e014927abfad1b7b6c5a436331e9c4
Harsh <hk2152573@gmail.com>
2023-04-01 23:04:05 +0530

    commit

--- src/pages/Skill/skill.js
+++ src/pages/Skill/skill.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './skill.css'
+import 'animate.css';
 
 export default function Skill() {
     return (
@@ -8,28 +9,28 @@ export default function Skill() {
                 <span>MY SKILLS</span>
             </div>
             <div className="skill-tray">
-                <div className="skill-box">
+                <div className="skill-box animate__animated animate__backInDown">
                     <div className="skill-percent-box">
                         <div className="fill"></div>
                         <div className="value">80%</div>
                     </div>
                     <h1>Front-end Developement</h1>
                 </div>
-                <div className="skill-box">
+                <div className="skill-box animate__animated animate__backInUp">
                     <div className="skill-percent-box">
                         <div className="fill"></div>
                         <div className="value">80%</div>
                     </div>
                     <h1>Back-end Developement</h1>
                 </div>
-                <div className="skill-box">
+                <div className="skill-box animate__animated animate__backInDown">
                     <div className="skill-percent-box">
                         <div className="fill"></div>
                         <div className="value">80%</div>
                     </div>
                     <h1> Developement</h1>
                 </div>
-                <div className="skill-box">
+                <div className="skill-box animate__animated animate__backInUp">
                     <div className="skill-percent-box">
                         <div className="fill"></div>
                         <div className="value">80%</div>
@@ -39,7 +40,40 @@ export default function Skill() {
             </div>
             <div className="skill-percent">
                 <div className="percent-box">
-                    <h3>Javascript - <span>(85%)</span></h3>
+                    <h3>Javascript - <span>(80%)</span></h3>
+                    <div className="percent-bar">
+                        <div className="percent-bar-fill" style={{ width: "80%" }}></div>
+                    </div>
+                </div>
+                <div className="percent-box">
+                    <h3>Node Js - <span>(60%)</span></h3>
+                    <div className="percent-bar">
+                        <div className="percent-bar-fill" style={{ width: "60%" }}></div>
+                    </div>
+                </div>
+                <div className="percent-box">
+                    <h3>React Js - <span>(80%)</span></h3>
+                    <div className="percent-bar">
+                        <div className="percent-bar-fill" style={{ width: "80%" }}></div>
+                    </div>
+                </div>
+                <div className="percent-box">
+                    <h3>HTML/CSS - <span>(80%)</span></h3>
+                    <div className="percent-bar">
+                        <div className="percent-bar-fill" style={{ width: "80%" }}></div>
+                    </div>
+                </div>
+                <div className="percent-box">
+                    <h3>Flutter - <span>(85%)</span></h3>
+                    <div className="percent-bar">
+                        <div className="percent-bar-fill" style={{ width: "85%" }}></div>
+                    </div>
+                </div>
+                <div className="percent-box">
+                    <h3>Programming Lang. - <span>(85%)</span></h3>
+                    <div className="percent-bar">
+                        <div className="percent-bar-fill" style={{ width: "85%" }}></div>
+                    </div>
                 </div>
             </div>
 
